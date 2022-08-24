ef67b1264c0c64fa592a7970fb032f10bb4be5fc
Ryan Hardin <rhardin23.rh@gmail.com>
2022-08-23 21:57:38 -0400

    added navbar with links

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Gallery from './pages/Gallery'
 
 function App() {
   return (
     <div className="App">
       <h1>Sayuni Investment LLC</h1>
+      <Navbar />
+      <div className='content'>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/gallery' element={<Gallery />} />
+        </Routes>
+      </div>
     </div>
   );
 }
