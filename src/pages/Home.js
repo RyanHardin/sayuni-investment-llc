44aef6c9f9fd03fd372d004b9782e91abb874185
Ryan Hardin <rhardin23.rh@gmail.com>
2022-08-25 11:13:40 -0400

    home page added with styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,19 @@
 import React from 'react'
-
+import image from '../assets/SAYUNI INVESTMENTS LLC.jpg'
+import './Home.css'
 function Home() {
     return (
-        <div>Home</div>
+        <div className='container'>
+            <div>
+                <img src={image} alt={"Logo"}></img>
+            </div>
+            <div className='info-item'>
+                <h1>Home Selling</h1>
+                <h2>Solutions</h2>
+                <h3>We make the property selling experience as easy as possible!</h3>
+                <button>Contact Us</button>
+            </div>
+        </div>
     )
 }
 
