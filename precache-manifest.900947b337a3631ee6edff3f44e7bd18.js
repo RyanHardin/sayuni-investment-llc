dd5b04a22037aa7a7e378286e781a820b9d12096
Ryan Hardin <rhardin23.rh@gmail.com>
2022-08-25 23:29:53 -0400

    Updates

---
+++ precache-manifest.900947b337a3631ee6edff3f44e7bd18.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "4971b9d24fad14a64c69",
+    "url": "RyanHardin.github.io/sayuni-investment-llc/static/css/main.1596e8e3.chunk.css"
+  },
+  {
+    "revision": "4971b9d24fad14a64c69",
+    "url": "RyanHardin.github.io/sayuni-investment-llc/static/js/main.69a8a395.chunk.js"
+  },
+  {
+    "revision": "39229e470bcc1ea415ff",
+    "url": "RyanHardin.github.io/sayuni-investment-llc/static/js/runtime~main.b1f2fad1.js"
+  },
+  {
+    "revision": "1e757b13da34b100f62a",
+    "url": "RyanHardin.github.io/sayuni-investment-llc/static/js/2.78595961.chunk.js"
+  },
+  {
+    "revision": "5245a74e29a16f7336f84d09b5fcba97",
+    "url": "RyanHardin.github.io/sayuni-investment-llc/static/media/SAYUNI INVESTMENTS LLC.5245a74e.jpg"
+  },
+  {
+    "revision": "b2b85366bc6402c78b14eb8bb72829f0",
+    "url": "RyanHardin.github.io/sayuni-investment-llc/static/media/Gavel.b2b85366.png"
+  },
+  {
+    "revision": "afecfc79611d86d3ede63bc2f04be9e2",
+    "url": "RyanHardin.github.io/sayuni-investment-llc/static/media/About Photo1.afecfc79.png"
+  },
+  {
+    "revision": "3e1e194b425eaf35e97796f9a6ee025e",
+    "url": "RyanHardin.github.io/sayuni-investment-llc/static/media/About Photo2.3e1e194b.png"
+  },
+  {
+    "revision": "5f16b8abe6b9fce90a5b38c4a8bf42af",
+    "url": "RyanHardin.github.io/sayuni-investment-llc/index.html"
+  }
+];
