aa52572ebfdb0be7a95415aaf2dd908309227c74
Ryan Hardin <rhardin23.rh@gmail.com>
2022-08-26 10:05:37 -0400

    Updates

---
+++ precache-manifest.f2b1588e33e704e1d89cfe5bb0cf36b6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "dcf8b1c40d28da131bec",
+    "url": "/static/css/main.1596e8e3.chunk.css"
+  },
+  {
+    "revision": "dcf8b1c40d28da131bec",
+    "url": "/static/js/main.c61f09d3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "38c62517c5db9397f97b",
+    "url": "/static/js/2.2ebb44e0.chunk.js"
+  },
+  {
+    "revision": "5245a74e29a16f7336f84d09b5fcba97",
+    "url": "/static/media/SAYUNI INVESTMENTS LLC.5245a74e.jpg"
+  },
+  {
+    "revision": "b2b85366bc6402c78b14eb8bb72829f0",
+    "url": "/static/media/Gavel.b2b85366.png"
+  },
+  {
+    "revision": "afecfc79611d86d3ede63bc2f04be9e2",
+    "url": "/static/media/About Photo1.afecfc79.png"
+  },
+  {
+    "revision": "3e1e194b425eaf35e97796f9a6ee025e",
+    "url": "/static/media/About Photo2.3e1e194b.png"
+  },
+  {
+    "revision": "8be70ebd87d6e85a96bce721be6685a9",
+    "url": "/index.html"
+  }
+];
