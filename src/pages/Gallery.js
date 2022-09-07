8dd21d2e5cba5d1409413dec5b5ef1fc0daa91ae
Ryan Hardin <rhardin23.rh@gmail.com>
2022-09-07 18:49:43 -0400

    carousel added to gallery

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import Carousel from 'react-bootstrap/Carousel';
+import house from '../assets/About Photo1.png'
+import logo from '../assets/SAYUNI INVESTMENTS LLC.jpg'
 
 function Gallery() {
     return (
-        <div></div>
+        <Carousel>
+            <Carousel.Item interval={5000}>
+                <img
+                    className="d-block w-100"
+                    src={logo}
+                    alt="House slide"
+                />
+            </Carousel.Item>
+            <Carousel.Item interval={5000}>
+                <img
+                    className="d-block w-100"
+                    src={house}
+                    alt="House slide"
+                />
+            </Carousel.Item>
+        </Carousel>
     )
 }
 
